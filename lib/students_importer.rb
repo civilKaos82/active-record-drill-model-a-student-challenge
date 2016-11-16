d3a66f7d1a1a4ebb1f111a8d7c2a1f0fbbd5f974
Torey Hickman <torey@toreyhickman.com>
2016-11-16 09:55:47 -0500

    Removes code for seeding the database
    The original code base included code
    for seeding the database with students.
    However, the challenge does not require
    seeding a database; students are only
    asked to build a class.

--- lib/students_importer.rb
+++
@@ -1,6 +0,0 @@
-module StudentsImporter
-  # If no filename is given, filename will point to the file in db/data/students.csv
-  def self.import(filename=File.dirname(__FILE__) + "/../db/data/students.csv")
-
-  end
-end
